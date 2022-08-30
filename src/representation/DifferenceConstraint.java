0632b6b7d497d81e0d396691f37b5e8d4128a181
Kali <antoine.collenot@gmail.com>
2022-08-30 11:22:10 -0400

    update

--- src/representation/DifferenceConstraint.java
+++ src/representation/DifferenceConstraint.java
@@ -5,10 +5,11 @@ import java.util.Set;
 
 public class DifferenceConstraint implements Constraint {
 
-    Variable v1;
-    Variable v2;
+    Variable v1,v2;
 
     public DifferenceConstraint(Variable v1, Variable v2) {
+        if v1.equals(v2)
+            throw new IllegalArgumentException("les variables v1 et v2 sont les mêmes");
         this.v1 = v1;
         this.v2 = v2;
     }
@@ -20,8 +21,9 @@ public class DifferenceConstraint implements Constraint {
         return scope;
     }
 
-    public boolean isSatisfiedBy(Map<Variable, Object> instance) {
-        
+    public boolean isSatisfiedBy(Map<Variable, object> instance) {
+        if !(instance.KeySet().contains(this.v1) && instace.KeySet().contains(this.v2))
+            throw new IllegalArgumentException("V1 or V2 aren't present in the given instance !");
     }
 
 }
