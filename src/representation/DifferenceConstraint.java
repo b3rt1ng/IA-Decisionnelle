d9c1f91bfe10dd9566d66731d265b96ce6dd0b5d
B3rt1ng <43414019+b3rt1ng@users.noreply.github.com>
2022-09-05 14:19:55 +0200

    hé gros ça y est

--- src/representation/DifferenceConstraint.java
+++ src/representation/DifferenceConstraint.java
@@ -2,13 +2,14 @@ package representation;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.HashSet;
 
 public class DifferenceConstraint implements Constraint {
 
-    Variable v1,v2;
+    Variable v1,v2; 
 
     public DifferenceConstraint(Variable v1, Variable v2) {
-        if v1.equals(v2)
+        if (v1.equals(v2))
             throw new IllegalArgumentException("les variables v1 et v2 sont les mêmes");
         this.v1 = v1;
         this.v2 = v2;
@@ -21,9 +22,9 @@ public class DifferenceConstraint implements Constraint {
         return scope;
     }
 
-    public boolean isSatisfiedBy(Map<Variable, object> instance) {
-        if !(instance.KeySet().contains(this.v1) && instace.KeySet().contains(this.v2))
+    public boolean isSatisfiedBy(Map<Variable, Object> instance) {
+        if (!(instance.keySet().contains(this.v1) && instance.keySet().contains(this.v2)))
             throw new IllegalArgumentException("V1 or V2 aren't present in the given instance !");
+        return !(instance.get(this.v1).equals(instance.get(this.v2)));
     }
-
 }
