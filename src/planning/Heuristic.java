bc84e23260d5dc53e7e9935d5146bed7c81b6324
Kali <antoine.collenot@gmail.com>
2022-09-20 10:53:28 -0400

    potite faute d'orthographe

--- src/planning/Heuristic.java
+++ src/planning/Heuristic.java
@@ -4,5 +4,5 @@ import representation.Variable;
 import java.util.Map;
 
 public interface Heuristic {
-    public abstract float estiamte(Map<Variable, Object> state); //estimation du coup d'un plan optimal jusqu'au but
+    public abstract float estimate(Map<Variable, Object> state); //estimation du coup d'un plan optimal jusqu'au but
 }
