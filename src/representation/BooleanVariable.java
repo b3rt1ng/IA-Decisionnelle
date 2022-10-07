df914aab9720d8f4fd85a916293732d5b3eddfd6
kenzo.lecoindre <kenzo.lecoindre@etu.unicaen.fr>
2022-10-07 18:34:30 +0200

    javadoc de BooleanVariable terminée

--- src/representation/BooleanVariable.java
+++ src/representation/BooleanVariable.java
@@ -2,8 +2,19 @@ package representation;
 
 import java.util.HashSet;
 
+/**
+ * Class representing a variable.
+ * 
+ * @author Kenzo LECOINDRE
+ */
 public class BooleanVariable extends Variable
 {
+    /**
+     * Initialise a new BooleanVariable given a name.
+     *
+     * @param name A String object representing a name.
+     * 
+     */
     public BooleanVariable(String name)
     {
         super(name, new HashSet<Object>());
