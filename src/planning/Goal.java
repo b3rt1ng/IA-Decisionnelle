86ea72dcc2b922a07e21120ab9c4cc4d5d5ab9f0
kenzo.lecoindre <kenzo.lecoindre@etu.unicaen.fr>
2022-10-07 19:10:14 +0200

    javadoc de Goal terminée

--- src/planning/Goal.java
+++ src/planning/Goal.java
@@ -4,7 +4,19 @@ import java.util.Map;
 
 import representation.Variable;
 
+
+/**
+ * Interface which contains all methods concerning a Goal.
+ * 
+ * @author Kenzo LECOINDRE
+ */
 public interface Goal 
 {
+    /**
+     * This method return true if the goal is satisfied by the given state, false otherwise.
+     * 
+     * @param state A state corresponding to a Map where each key is a Variable and each value is these value.
+     * @return true if the goal is satisfied by the given state, false otherwise.
+     */
     public abstract boolean isSatisfiedBy(Map<Variable, Object> state);
 }
