f1e4290141fbc1f71e87e8c43a349dbe71919a99
kenzo.lecoindre <kenzo.lecoindre@etu.unicaen.fr>
2022-10-09 16:29:14 +0200

    javadoc de DFSPlanner terminée

--- src/planning/DFSPlanner.java
+++ src/planning/DFSPlanner.java
@@ -8,13 +8,41 @@ import java.util.Set;
 
 import representation.Variable;
 
+/**
+ * Class representing a planner using Depth-first search.
+ * 
+ * @author Kenzo LECOINDRE
+ */
 public class DFSPlanner implements Planner
 {
+    /**
+     * State representing the initial state.
+     */
     private Map<Variable, Object> initialState;
+
+    /**
+     * Set of actions representing all the differents actions allowed to be used.
+     */
     private Set<Action> actions;
+
+    /**
+     * Goal representing the goal to reach.
+     */
     private Goal goal;
+
+    /**
+     * A counter to count the number of nodes visited by the last planning.
+     */
     private int nbNodes;
 
+    /**
+     * Initialise a new DFSPlanner given an initial state, a set of actions allowed and a goal.
+     *
+     * @param initialState A Map where each key is a Variable and each value is these value representing an initial state.
+     * @param actions A Set of actions representing the list of actions allowed to be used.
+     * @param goal A goal representing the goal to to reach.
+     * 
+     */
     public DFSPlanner(Map<Variable, Object> initialState, Set<Action> actions, Goal goal)
     {
         this.initialState = initialState;
@@ -23,6 +51,15 @@ public class DFSPlanner implements Planner
         this.nbNodes = 0;
     }
 
+    /**
+     * Launch a dfs search
+     * 
+     * @param state A Map where each key is a Variable and each value is the value, representing a state.
+     * @param result A list of actions representing the actions to do to go to the given state.
+     * @param alreadyExplored A set of states already explored by the function.
+     * 
+     * @return A list of actions representing the actions to do to go to the goal from the initial state given.
+     */
     private List<Action> dfs(Map<Variable, Object> state, List<Action> result, Set<Map<Variable, Object>> alreadyExplored)
     {
         if(this.goal.isSatisfiedBy(state))
@@ -62,6 +99,15 @@ public class DFSPlanner implements Planner
     @Override
     public int getNbNodes() { return this.nbNodes; }
 
+    /**
+     * The toString function returns a string representation of the object.
+     * This is useful for debugging purposes, and can be used to print out an
+     * object or part of an object.  This function returns a String that contains
+     * the string representation of all the attribute of a dfs planner.
+     * 
+    * @return A String representation of the DFSPlanner.
+    * 
+    */
     @Override
     public String toString()
     {
