2e81e7b09e1e5b9f19d91f58a4e165147e70a25e
b3rt1ng <antoine.collenot@gmail.com>
2022-11-11 13:16:59 -0500

    datamining plus qu'une classe

---
+++ src/datamining/AssociationRuleMiner.java
@@ -0,0 +1,21 @@
+package datamining;
+
+import java.util.Set;
+
+/**
+ * interface representing a miner of association rules
+ *
+ * @author Antoine Collenot, Kenzo Lecoindre
+ */
+
+public interface AssociationRuleMiner {
+    
+    public BooleanDatabase getDataBase();
+
+    /**
+     * compute the set of frequency and confidence association rules above the thresholds given
+     * @param minFrequency the minimum frequency threshold
+     * @return the set of association rules above the given thresholds
+     */
+    public Set<AssociationRule> extract(float minConfidence);
+}
