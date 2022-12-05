1156ac6e31e53417e0a11c679eee54ba24f15004
b3rt1ng <antoine.collenot@gmail.com>
2022-12-05 09:07:24 -0500

    monde avec contrintes régulières + correction de bugs

---
+++ src/blocksworld/representation/WorldWithRegularConstraints.java
@@ -0,0 +1,53 @@
+package blocksworld.representation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import representation.Implication;
+import representation.Variable;
+import representation.Constraint;
+
+public class WorldWithRegularConstraints extends WorldWithConstraint {
+
+    protected Set<Constraint> regularConstraints;
+
+    /**
+     * Initialise a new World given the number of blocks and the number of stacks.
+     * 
+     * @param nbBlocks the number of blocks in the world.
+     * @param nbStacks the number of stacks in the world.
+     */
+    public WorldWithRegularConstraints(int nbBlocks, int nbStacks) {
+        super(nbBlocks, nbStacks);
+
+        this.regularConstraints = new HashSet<>();
+
+        Set<Integer> ensDom = new HashSet<>();
+        for (Integer val : this.freeP.keySet()) {
+            ensDom.add(val);
+        }
+
+        for(Integer i : this.onB.keySet()) {
+            Variable v = this.onB.get(i);
+            for (Object o: v.getDomain()){
+                Integer ni = (Integer) o;
+
+                Integer mustBe = ni + (ni - i);
+                if (ni >= 0) {
+                    Variable vj = this.onB.get(ni);
+                    Set<Object> dom = new HashSet<>(ensDom);
+                    if (mustBe >= 0 && mustBe < this.nbBlocks) {
+                        dom.add(mustBe);
+                    }
+                    Set<Object> firstDom = Set.of(ni);
+                    this.regularConstraints.add(new Implication(v, firstDom, vj, dom));
+                }
+            }
+        }
+    }
+
+    @Override
+    public Set<Constraint> getConstraints() {
+        return this.regularConstraints;
+    }
+}
